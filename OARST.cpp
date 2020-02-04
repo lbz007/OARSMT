fd4cbbe70f31bf934f9cd5b2377168fbdb0ba384
lbz007 <49712344+lbz007@users.noreply.github.com>
2020-02-04 11:02:32 +0800

    Revert "Revert "Add files via upload""

--- OARST.cpp
+++ OARST.cpp
@@ -1,8 +1,47 @@
 #include "global.h"
 
+// int whichregion(poi a,poi b)
+// {
+//     if (a.x==b.x || a.y==b.y) return 0;
+//     if (a.x<b.x && a.y<b.y) return 1;
+//     if (a.x>b.x && a.y<b.y) return 2;
+//     if (a.x>b.x && a.y>b.y) return 3;
+//     if (a.x<b.x && a.y>b.y) return 4;
+//     return 0;
+// }
+// int calcgain(poi p0,poi p1,poi p2)
+// {
+//     int r1,r2;
+//     r1=whichregion(p0,p1);
+//     r2=whichregion(p0,p2);
+//     if (r1==0 || r2==0)
+//         return 0;
+//     if (abs(r1-r2)==2)
+//         return 0;
+//     if (abs(r1-r2)==0)
+//         return (abs(p0.y-p1.y)+abs(p0.x-p2.x));
+//     if (abs(r1-r2)==1||abs(r1-r2)==3)
+//     {
+//         if (r1+r2==5)
+//         {
+//             if (abs(p1.x-p0.x)>abs(p2.x-p0.x)) swap(p1,p2);
+//             return (abs(p1.x-p0.x));
+//         }
+//         if (r1+r2!=5)
+//         {
+//             if (abs(p1.y-p0.y)>abs(p2.y-p0.y)) swap(p1,p2);
+//             return (abs(p1.y-p0.y));
+//         }
+//     }
+//     return 0;
+// }
 int whichregion(poi a,poi b)
 {
-    if (a.x==b.x || a.y==b.y) return 0;
+    if (a.x==b.x && a.y==b.y) return 0;
+    if (a.y==b.y && b.x>a.x) return 5; //4 1
+    if (a.x==b.x && b.y>a.y) return 6; //1 2
+    if (a.y==b.y && b.x<a.x) return 7; //2 3
+    if (a.x==b.x && b.y<a.y) return 8; //3 4
     if (a.x<b.x && a.y<b.y) return 1;
     if (a.x>b.x && a.y<b.y) return 2;
     if (a.x>b.x && a.y>b.y) return 3;
@@ -11,26 +50,52 @@ int whichregion(poi a,poi b)
 }
 int calcgain(poi p0,poi p1,poi p2)
 {
-    int r1,r2;
+    int r1,r2,px,py;
     r1=whichregion(p0,p1);
     r2=whichregion(p0,p2);
     if (r1==0 || r2==0)
         return 0;
+    if (r1>4 || r2>4)
+    {
+        if (r1>4 && r2>4) return 0;
+        if (r2>4) swap(p1,p2);
+        if (r1==5 && (r2==1 || r2==4))
+            return abs(p1.x-p0.x)<abs(p2.x-p0.x)?abs(p1.x-p0.x):abs(p2.x-p0.x);
+        if (r1==7 && (r2==2 || r2==3))
+            return abs(p1.x-p0.x)<abs(p2.x-p0.x)?abs(p1.x-p0.x):abs(p2.x-p0.x);
+        if (r1==6 && (r2==1 || r2==2))
+            return abs(p1.y-p0.y)<abs(p2.y-p0.y)?abs(p1.y-p0.y):abs(p2.y-p0.y);
+        if (r1==8 && (r2==3 || r2==4))
+            return abs(p1.y-p0.y)<abs(p2.y-p0.y)?abs(p1.y-p0.y):abs(p2.y-p0.y);  
+        return 0; 
+    }
     if (abs(r1-r2)==2)
         return 0;
     if (abs(r1-r2)==0)
-        return (abs(p0.y-p1.y)+abs(p0.x-p2.x));
+    {
+        if (abs(p1.x-p0.x)<abs(p2.x-p0.x)) px=p1.x;
+            else px=p2.x;
+            if (abs(p1.y-p0.y)<abs(p2.y-p0.y)) py=p1.y;
+            else py=p2.y;
+        if (ispointblocked({px,py})==0)
+            return (abs(p0.y-p1.y)+abs(p0.x-p2.x));
+        else return 0;
+    }
     if (abs(r1-r2)==1||abs(r1-r2)==3)
     {
         if (r1+r2==5)
         {
             if (abs(p1.x-p0.x)>abs(p2.x-p0.x)) swap(p1,p2);
-            return (abs(p1.x-p0.x));
+                if (ispointblocked({p1.x,p0.y})==0)
+                    return (abs(p1.x-p0.x));
+                else return 0; 
         }
         if (r1+r2!=5)
         {
             if (abs(p1.y-p0.y)>abs(p2.y-p0.y)) swap(p1,p2);
-            return (abs(p1.y-p0.y));
+                if (ispointblocked({p0.x,p1.y})==0)
+                    return (abs(p1.y-p0.y));
+                else return 0;    
         }
     }
     return 0;
@@ -104,27 +169,49 @@ void OARST()
         else p2=b[MTSTedge[maxid].u];
         r1=whichregion(p0,p1);
         r2=whichregion(p0,p2);
-        if (r1==0 || r2==0)
+        if (r1>4 || r2>4)
         {
-            OARSTedge[++numOARSTe]={p1,p0};
-            OARSTedge[++numOARSTe]={p2,p0};
+            if (r2>4) 
+            {
+                swap(p1,p2);
+                swap(r1,r2);
+            }
+            if ((r1==5 && (r2==1 || r2==4)) || (r1==7 && (r2==2 || r2==3)))
+                if (abs(p1.x-p0.x)<abs(p2.x-p0.x))
+                {
+                    OARSTedge[++numOARSTe]={p0,p1};
+                    OARSTedge[++numOARSTe]={p1,p2};
+                }
+                else
+                {
+                    OARSTedge[++numOARSTe]={p0,{p2.x,p0.y}};
+                    OARSTedge[++numOARSTe]={p1,{p2.x,p0.y}};
+                    OARSTedge[++numOARSTe]={p2,{p2.x,p0.y}};
+                }
+            if ((r1==6 && (r2==1 || r2==2)) || (r1==8 && (r2==3 || r2==4)))
+                if (abs(p1.y-p0.y)<abs(p2.y-p0.y))
+                {
+                    OARSTedge[++numOARSTe]={p0,p1};
+                    OARSTedge[++numOARSTe]={p1,p2};
+                }
+                else
+                {
+                    OARSTedge[++numOARSTe]={p0,{p0.x,p2.y}};
+                    OARSTedge[++numOARSTe]={p1,{p0.x,p2.y}};
+                    OARSTedge[++numOARSTe]={p2,{p0.x,p2.y}};
+                }
             continue;
         }
-        if (abs(r1-r2)==2)
-        {
-            OARSTedge[++numOARSTe]={p1,{p0.x,p1.y}};
-            OARSTedge[++numOARSTe]={p0,{p0.x,p1.y}};
-            OARSTedge[++numOARSTe]={p2,{p0.x,p2.y}};
-            OARSTedge[++numOARSTe]={p0,{p0.x,p2.y}};       
-        }
+
         if (abs(r1-r2)==0)
         {
             if (abs(p1.x-p0.x)<abs(p2.x-p0.x)) px=p1.x;
             else px=p2.x;
             if (abs(p1.y-p0.y)<abs(p2.y-p0.y)) py=p1.y;
             else py=p2.y;
-            OARSTedge[++numOARSTe]={p0,{px,p0.y}};
-            OARSTedge[++numOARSTe]={{px,py},{px,p0.y}};
+            //OARSTedge[++numOARSTe]={p0,{px,p0.y}};
+            //OARSTedge[++numOARSTe]={{px,py},{px,p0.y}};
+            OARSTedge[++numOARSTe]={p0,{px,py}};
             OARSTedge[++numOARSTe]={{px,py},p1};
             OARSTedge[++numOARSTe]={{px,py},p2};
         }
@@ -135,21 +222,23 @@ void OARST()
                 if (abs(p1.x-p0.x)>abs(p2.x-p0.x)) swap(p1,p2);
                 OARSTedge[++numOARSTe]={p0,{p1.x,p0.y}};
                 OARSTedge[++numOARSTe]={p1,{p1.x,p0.y}};
-                OARSTedge[++numOARSTe]={{p1.x,p0.y},{p2.x,p0.y}};
-                OARSTedge[++numOARSTe]={p2,{p2.x,p0.y}};
+                // OARSTedge[++numOARSTe]={{p1.x,p0.y},{p2.x,p0.y}};
+                // OARSTedge[++numOARSTe]={p2,{p2.x,p0.y}};
+                OARSTedge[++numOARSTe]={p2,{p1.x,p0.y}};
             }
             if (r1+r2!=5)
             {
                 if (abs(p1.y-p0.y)>abs(p2.y-p0.y)) swap(p1,p2);
                 OARSTedge[++numOARSTe]={p0,{p0.x,p1.y}};
                 OARSTedge[++numOARSTe]={p1,{p0.x,p1.y}};
-                OARSTedge[++numOARSTe]={{p0.x,p1.y},{p0.x,p2.y}};
-                OARSTedge[++numOARSTe]={p2,{p0.x,p2.y}};
+                // OARSTedge[++numOARSTe]={{p0.x,p1.y},{p0.x,p2.y}};
+                // OARSTedge[++numOARSTe]={p2,{p0.x,p2.y}};
+                OARSTedge[++numOARSTe]={p2,{p0.x,p1.y}};
             }
         }
-
     }
+    WL=0;
     for(i=1;i<=numOARSTe;i++)
         WL+=disp(OARSTedge[i].u,OARSTedge[i].v);
-    printf("WL: %d\n",WL);
+    printf("OARST WL: %d\n",WL);
 }
--- func.cpp
+++ func.cpp
@@ -12,9 +12,25 @@ bool cmpp(poi a,poi b)
         return a.x<b.x;
     }
 }
+bool cmpedge2(edge2 a,edge2 b)
+{
+    if (equpoi(a.u,b.u)) return cmpp(a.v,b.v);
+    else
+    {
+        return cmpp(a.u,b.u);
+    }
+}
 bool equpoi(poi a,poi b)
 {
     return (a.x==b.x && a.y==b.y);
+}
+bool equedge2(edge2 a,edge2 b)
+{
+    if (equpoi(a.u,b.u) && equpoi(a.v,b.v))
+        return 1;
+    if (equpoi(a.u,b.v) && equpoi(a.v,b.u))
+        return 1;
+    return 0;
 }
 bool cmpedge(edge a,edge b)
 {
@@ -34,6 +50,10 @@ void add(int u,int v,int c)
 {
     e[++ee]=v;pre[head[u]]=ee;nex[ee]=head[u];head[u]=ee;len[ee]=c;
 }
+void add2(int u,int v)
+{
+    e2[++ee2]=v;nex2[ee2]=head2[u];head2[u]=ee2;
+}
 void deledge(int u,int ee)
 {
     pre[nex[ee]]=pre[ee];
@@ -48,6 +68,11 @@ void deledge(int u,int ee)
     /* else nex[pre[ee]]=nex[ee]; */
 
 }
+int opedge(int x)
+{
+    if (x%2==0) return x-1;
+    else return x+1; 
+}
 bool inbox(poi p,poi p1,poi p2)
 {
     if (p.x>=p2.x || p.x<=p1.x || p.y>=p2.y || p.y<=p1.y)
@@ -61,6 +86,13 @@ void initedge()
     memset(pre,0,sizeof(pre));
     memset(vis,0,sizeof(vis));
     ee=0;
+}
+void initedge2()
+{
+    memset(head2,0,sizeof(head2));
+    memset(nex2,0,sizeof(nex2));
+    memset(vis2,0,sizeof(vis2));
+    ee2=0;
 }
 int find(int x)
 {
@@ -81,4 +113,80 @@ void init()
         mp.insert(pair<poi,int>({oo[i].x2,oo[i].y1,0},++n));
         mp.insert(pair<poi,int>({oo[i].x2,oo[i].y2,0},++n));
     }
+}
+void refineedge(int &n, edge2 a[])
+{
+    int i;
+    vector<edge2> po;
+    for (i=1;i<=n;i++)
+    {
+        if (equpoi(a[i].u,a[i].v))
+            continue;
+        if (cmpp(a[i].u,a[i].v))
+            swap(a[i].u,a[i].v);
+        po.push_back(a[i]);
+    }
+    sort(po.begin(),po.end(),cmpedge2);
+    vector<edge2>::iterator new_end=unique(po.begin(),po.end(),equedge2);
+    po.erase(new_end,po.end());
+    n=po.size();
+    for (i=0;i<n;i++)
+    {
+        a[i+1]=po[i];
+    }        
+}
+
+void blockedgepoint(poi u,poi v,obs o,poi &pa,poi &pb)
+{
+    poi paa,pbb;
+    if (o.x2-o.x1<abs(u.x-v.x))
+    {
+        pa={o.x1,o.y2};
+        pb={o.x2,o.y2};
+        if (disp(pa,{u.x,u.y,0})>disp(pb,{u.x,u.y,0}))
+            swap(pa,pb);
+        paa={o.x1,o.y1};
+        pbb={o.x2,o.y1};
+        if (disp(paa,{u.x,u.y,0})>disp(pbb,{u.x,u.y,0}))
+            swap(paa,pbb);
+        if (disp(paa,{u.x,u.y,0})+disp(pbb,{v.x,v.y,0})<disp(pa,{u.x,u.y,0})+disp(pb,{v.x,v.y,0}))
+        {
+            pa=paa;
+            pb=pbb;
+        }
+    }
+    else
+    {
+        pa={o.x1,o.y1};
+        pb={o.x1,o.y2};
+        if (disp(pa,{u.x,u.y,0})>disp(pb,{u.x,u.y,0}))
+            swap(pa,pb);
+        paa={o.x2,o.y1};
+        pbb={o.x2,o.y2};
+        if (disp(paa,{u.x,u.y,0})>disp(pbb,{u.x,u.y,0}))
+            swap(paa,pbb);
+        if (disp(paa,{u.x,u.y,0})+disp(pbb,{v.x,v.y,0})<disp(pa,{u.x,u.y,0})+disp(pb,{v.x,v.y,0}))
+        {
+            pa=paa;
+            pb=pbb;
+        }
+    }    
+}
+bool ispointblocked(poi u)
+{
+    ansob.clear();
+    checknode(u,1);
+    if (ansob.size()==0)
+        return 0;
+    else
+        return 1;
+}
+bool isedgeblocked(poi u,poi v)
+{
+    ansob.clear();
+    checkedge(u,v,1);
+    if (ansob.size()==0)
+        return 0;
+    else
+        return 1;
 }
